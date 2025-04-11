7cdb8be2b3726be5c7a30cd030f3fe003fe510ac
Felipe Fabossi <45209862+fabossi@users.noreply.github.com>
2025-04-11 14:59:44 -0300

    Initial Commit

---
+++ RoleConnect/RoleConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoleConnectApp.swift
+//  RoleConnect
+//
+//  Created by Felipe Fabossi on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RoleConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
